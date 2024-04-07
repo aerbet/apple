954a7046b828fcd4919d5df15b89934a640dcb13
Erbol Zhaparov <ae.270718@gmail.com>
2024-04-07 15:22:56 +0600

    created components, added utils and constants

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { appleImg } from "../utils";
+
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="appleImg"/>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
